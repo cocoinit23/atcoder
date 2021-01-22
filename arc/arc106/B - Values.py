b0850ba562b9ef88f4f969421e87593a4f322aac
cocoinit23 <coco.oshi@gmail.com>
2021-01-22 15:24:32 +0900

    commit

---
+++ arc/arc106/B - Values.py
@@ -0,0 +1,59 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [-1] * (n + 1)
+        self.rank = [0] * (n + 1)
+
+    def find(self, x):
+        if self.parent[x] < 0:
+            return x
+        else:
+            self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+        elif self.rank[x] > self.rank[y]:
+            self.parent[x] += self.parent[y]
+            self.parent[y] = x
+        else:
+            self.parent[y] += self.parent[x]
+            self.parent[x] = y
+            if self.rank[x] == self.rank[y]:
+                self.rank[y] += 1
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def count(self, x):
+        return -self.parent[self.find(x)]
+
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+uf = UnionFind(n)
+for _ in range(m):
+    c, d = map(int, input().split())
+    uf.union(c, d)
+
+aa = [0] * (n + 1)
+bb = [0] * (n + 1)
+
+for i in range(n):
+    r = uf.find(i + 1)
+    aa[r] += a[i]
+    bb[r] += b[i]
+
+ans = True
+for i in range(n):
+    r = uf.find(i + 1)
+    if aa[r] != bb[r]:
+        ans = False
+        break
+
+print("Yes") if ans else print("No")
